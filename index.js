6ae26663e228a98f44dd8f091632263a06e21648
Joseph Cillo <jcillo@Josephs-MacBook-Pro.local>
2019-07-12 10:46:42 -0400

    car stays on screen

--- index.js
+++ index.js
@@ -1,26 +1,50 @@
+// moves car
 const playerCar = { x: 0, y: 0 };
+const obstacleCars = {};
+
+const isOnRoad = (x, y) => {
+  if (x < 0 || y < 0 || x > 62 || y > 24) {
+    return false;
+  }
+  return true;
+};
+
+const onScreen = (x, y) => {
+  if (!isOnRoad(x, y)) {
+    return false;
+  }
+  return true;
+};
 
 const playerMove = document.querySelector('.playerCar');
-function movePlayerCar(x, y) {
-  playerMove.style.left = `${(playerCar.x * 10).toString()}px`;
-  playerMove.style.top = `${(playerCar.y * 10).toString()}px`;
+function movePlayerCar() {
+  playerMove.style.left = `${(playerCar.x * 20).toString()}px`;
+  playerMove.style.top = `${(playerCar.y * 20).toString()}px`;
 }
 
 function moveDown() {
-  playerCar.y += 1;
-  movePlayerCar(playerCar.x, playerCar.y);
+  if (onScreen(playerCar.x, playerCar.y + 1)) {
+    playerCar.y += 1;
+    movePlayerCar(playerCar.x, playerCar.y);
+  }
 }
 function moveLeft() {
-  playerCar.x -= 1;
-  movePlayerCar(playerCar.x, playerCar.y);
+  if (onScreen(playerCar.x - 1, playerCar.y)) {
+    playerCar.x -= 1;
+    movePlayerCar(playerCar.x, playerCar.y);
+  }
 }
 function moveUp() {
-  playerCar.y -= 1;
-  movePlayerCar(playerCar.x, playerCar.y);
+  if (onScreen(playerCar.x, playerCar.y - 1)) {
+    playerCar.y -= 1;
+    movePlayerCar(playerCar.x, playerCar.y);
+  }
 }
 function moveRight() {
-  playerCar.x += 1;
-  movePlayerCar(playerCar.x, playerCar.y);
+  if (onScreen(playerCar.x + 1, playerCar.y)) {
+    playerCar.x += 1;
+    movePlayerCar(playerCar.x, playerCar.y);
+  }
 }
 window.addEventListener('keydown', (evt) => {
   if ([37, 38, 39, 40].includes(evt.keyCode)) {
@@ -42,3 +66,11 @@ window.addEventListener('keydown', (evt) => {
     default:
   }
 });
+// creates obstacles
+if (obstacleCars.length < 3) {
+  for (let i = 0; i < obstacleCars.length; i += 1) {
+    const newObstacle = document.createElement('div');
+    newObstacle.classList.add('obstacleCar');
+    document.querySelector('.road').appendChild(newObstacle);
+  }
+}
