b76f05f8297df3661c8f90c025d1961731b09b57
Joseph Cillo <jcillo@Josephs-MacBook-Pro.local>
2019-07-15 15:40:27 -0400

    collision detetion'

--- index.js
+++ index.js
@@ -1,71 +1,159 @@
 /* eslint - disable; */
 
 const playerCar = {
-  x: 32, y: 12, height: '20vh', width: '5vh',
+  x: 32, y: 12, height: '180px', width: '45px',
 };
+
+
+const obstacleCars = [];
 
 const playerMove = document.querySelector('.playerCar');
 
 playerMove.style.left = '650px';
 playerMove.style.top = '240px';
+const isThereObstacle = (obstacleCar) => {
+  const playerX = parseInt(playerCar.x, 10);
+  const playerY = parseInt(playerCar.y, 10);
+  const playerH = parseInt(playerCar.height, 10);
+  const playerW = parseInt(playerCar.width, 10);
+  const obstacleX = parseInt(obstacleCar.style.left, 10);
+  const obstacleY = parseInt(obstacleCar.style.top, 10);
+  const obstacleH = parseInt(obstacleCar.style.height, 10);
+  const obstacleW = parseInt(obstacleCar.style.width, 10);
 
-const createBlueObstacle = () => {
+  if (
+    (playerX * 20 < obstacleX + obstacleW)
+    && ((playerX * 20) + playerW > obstacleX)
+    && ((playerY * 20) < obstacleY + obstacleH)
+    && ((playerY * 20) + playerH > obstacleY)
+
+  ) {
+    console.log('working');
+    return true;
+  }
+  return false;
+};
+
+const createBlue = () => {
   const obstacleCar = document.createElement('div');
-  obstacleCar.classList.add('obstacleCar');
-  obstacleCar.style.left = '625px';
+  obstacleCar.classList.add('blueCar');
+  obstacleCar.style.left = `${Math.random() * 1424}px`;
+  obstacleCar.style.height = '180px';
+  obstacleCar.style.width = '45px';
   document.querySelector('.road').appendChild(obstacleCar);
+  obstacleCars.push(obstacleCar);
 
-  let position = 0;
+  obstacleCar.y = 0;
   const obstacleCarInterval = setInterval(
     () => {
-      if (position === 625) {
+      if (obstacleCar.y === 625) {
         obstacleCar.remove();
         clearInterval(obstacleCarInterval);
+        obstacleCars.splice(obstacleCar);
       } else {
-        position += 1;
-        obstacleCar.style.top = `${position}px`;
+        isThereObstacle(obstacleCar);
+        obstacleCar.y += 1;
+        obstacleCar.style.top = `${obstacleCar.y}px`;
       }
     },
     1,
   );
-  const collisionDetection = () => {
-    if (
-      playerCar.x < obstacleCar.x
-        + parseInt(obstacleCar.width, 10)
-        && parseInt(playerCar.width, 10) + playerCar.x
-        > obstacleCar.x
-        && playerCar.y < obstacleCar.y
-        + parseInt(obstacleCar.height, 10)
-        && playerCar.y + parseInt(playerCar.height, 10)
-        > obstacleCar.y
-    ) {
-      console.log('working');
-    }
-  };
-  collisionDetection();
-  console.log(playerCar.x)
-  console.log(obstacleCar.x)
+};
+const createWhite = () => {
+  const obstacleCar = document.createElement('div');
+  obstacleCar.classList.add('whiteCar');
+  obstacleCar.style.left = `${Math.random() * 1424}px`;
+  obstacleCar.style.height = '180px';
+  obstacleCar.style.width = '45px';
+  document.querySelector('.road').appendChild(obstacleCar);
+  obstacleCars.push(obstacleCar);
+
+  obstacleCar.y = 0;
+  const obstacleCarInterval = setInterval(
+    () => {
+      if (obstacleCar.y === 625) {
+        obstacleCar.remove();
+        clearInterval(obstacleCarInterval);
+        obstacleCars.splice(obstacleCar);
+      } else {
+        isThereObstacle(obstacleCar);
+        obstacleCar.y += 1;
+        obstacleCar.style.top = `${obstacleCar.y}px`;
+      }
+    },
+    1,
+  );
+};
+const createRed = () => {
+  const obstacleCar = document.createElement('div');
+  obstacleCar.classList.add('redCar');
+  obstacleCar.style.left = `${Math.random() * 1424}px`;
+  obstacleCar.style.height = '180px';
+  obstacleCar.style.width = '45px';
+  document.querySelector('.road').appendChild(obstacleCar);
+  obstacleCars.push(obstacleCar);
+
+  obstacleCar.y = 0;
+  const obstacleCarInterval = setInterval(
+    () => {
+      if (obstacleCar.y === 625) {
+        obstacleCar.remove();
+        clearInterval(obstacleCarInterval);
+        obstacleCars.splice(obstacleCar);
+      } else {
+        isThereObstacle(obstacleCar);
+        obstacleCar.y += 2;
+        obstacleCar.style.top = `${obstacleCar.y}px`;
+      }
+    },
+    1,
+  );
+};
+const createGreen = () => {
+  const obstacleCar = document.createElement('div');
+  obstacleCar.classList.add('greenCar');
+  obstacleCar.style.left = `${Math.random() * 1424}px`;
+  obstacleCar.style.height = '180px';
+  obstacleCar.style.width = '45px';
+  document.querySelector('.road').appendChild(obstacleCar);
+  obstacleCars.push(obstacleCar);
+
+  obstacleCar.y = 0;
+  const obstacleCarInterval = setInterval(
+    () => {
+      if (obstacleCar.y === 625) {
+        obstacleCar.remove();
+        clearInterval(obstacleCarInterval);
+        obstacleCars.splice(obstacleCar);
+      } else {
+        isThereObstacle(obstacleCar);
+        obstacleCar.y += 2;
+        obstacleCar.style.top = `${obstacleCar.y}px`;
+      }
+    },
+    1,
+  );
 };
 
 
 const continousObstacleCars = () => {
   setInterval(() => {
-    createBlueObstacle();
+    createBlue();
+  }, 1000);
+  setInterval(() => {
+    createWhite();
   }, 3000);
-  // setInterval(() => {
-  //   createBlackObstacle();
-  // }, 3000);
-  // setInterval(() => {
-  //   createGreenObstacle();
-  // }, 3000);
-  // setInterval(() => {
-  //   createYellowObstacle();
-  // }, 3000);
+  setInterval(() => {
+    createRed();
+  }, 2000);
+  setInterval(() => {
+    createGreen();
+  }, 4000);
 };
 continousObstacleCars();
 
 const inGrid = (x, y) => {
-  if (x < 0 || y < 0 || x > 67 || y > 24) {
+  if (x < 0 || y < 0 || x > 67 || y > 27) {
     return false;
   }
   return true;
@@ -90,7 +178,7 @@ const moveDown = () => {
 };
 const moveLeft = () => {
   if (onScreen(playerCar.x - 1, playerCar.y)) {
-    playerCar.x -= 1;
+    playerCar.x -= 4;
     movePlayerCar(playerCar.x, playerCar.y);
   }
 };
@@ -102,7 +190,7 @@ const moveUp = () => {
 };
 const moveRight = () => {
   if (onScreen(playerCar.x + 1, playerCar.y)) {
-    playerCar.x += 1;
+    playerCar.x += 4;
     movePlayerCar(playerCar.x, playerCar.y);
   }
 };
@@ -126,3 +214,10 @@ window.addEventListener('keydown', (evt) => {
     default:
   }
 });
+
+
+let timeNow = 60;
+setInterval(() => {
+  timeNow -= 1;
+  document.querySelector('.timer').innerHTML = timeNow;
+}, 1000);
